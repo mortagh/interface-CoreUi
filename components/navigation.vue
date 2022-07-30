efaab2d9b7c6782722cafd8b8345c7527abb6aeb
AxelGeremia <ax.geremia@gmail.com>
2022-07-30 08:24:48 +0200

    mise en place nav bar

---
+++ components/navigation.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app class="primary ">
+        <v-list-item>
+            <v-list-item-content>
+                <v-list-item-title class="white--text text-h6">
+                    <p> {{ title }} </p>
+                </v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+      <v-list>
+        <v-list-item v-for="(item, i) in nav" :key="i" :to="item.to" router exact
+            class="secondary greyTextNavBar--text">
+            <v-list-item-icon v-if="item.icon">
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+
+            <v-list-item v-for="(category,i) in item.categories" :key="i">
+                <v-list-item-content>
+                    <v-list-item-title v-text="category.title" />
+                </v-list-item-content>
+
+                <!-- <v-list-item v-for="(children,i) in category.children" :key="i" :to="children.to">
+                    <v-list-item-action>
+                        <v-icon>{{ children.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title v-text="children.title" />
+                    </v-list-item-content>
+                </v-list-item> -->
+            </v-list-item>
+        </v-list-item>
+        <v-btn icon @click.stop="miniVariant = !miniVariant">
+          <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        </v-btn>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "navigation",
+    data() {
+      return {
+        drawer: false,
+        clipped: false,
+        miniVariant: false,
+        title: 'CoreUI',
+        iconTitle: 'mdi-alpha-c-circle-outline',
+        nav: [{
+            title: 'Dashboard',
+            icon: 'mdi-view-dashboard-outline',
+            to: '/',
+          },
+          {
+            categories: [{
+                title: 'theme',
+                children: [{
+                    title: 'Color',
+                    icon: 'mdi-water-outline',
+                    to: '/inspire',
+                  },
+                  {
+                    title: 'Typography',
+                    icon: 'mdi-pencil-outline',
+                    to: '/#',
+                  }
+                ],
+              },
+              {
+                title: 'Components',
+                children: [{
+                  title: 'Base',
+                  icon: 'mdi-puzzle'
+                }]
+              }
+            ],
+          }
+        ],
+      }
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <Navigation />
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import navigation from '~/components/navigation.vue'
+export default {
+  components: { navigation },
+  name: 'DefaultLayout',
+  data () {
+    return {
+      drawer: false,
+      clipped: false,
+      fixed: false,
+      right: true,
+      rightDrawer: false,
+    }
+  }
+}
+</script>
